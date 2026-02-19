d87e6ae80d776cfff9452f97a5bf49d0b33ca2a5
Poppy Carpenter <poppy.carpenter@ada.ac.uk>
2026-02-19 13:10:14 +0000

    chore: use updated ACCOUNT_SIGNUP_FIELDS config & autoformat

--- musicplayer_project/settings.py
+++ musicplayer_project/settings.py
@@ -26,73 +26,71 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = os.getenv("DEBUG", "False")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 hostname = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if hostname:
     ALLOWED_HOSTS.append(hostname)
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'musicplayer',
-    'corsheaders',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'rest_framework_simplejwt',
-    'dj_rest_auth',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "musicplayer",
+    "corsheaders",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
+    "dj_rest_auth",
     # Registration dependencies
-    'django.contrib.sites',
-    'allauth.socialaccount',
-    'allauth',
-    'allauth.account', 
-    'dj_rest_auth.registration',
-    "anymail", # For email verification
-
+    "django.contrib.sites",
+    "allauth.socialaccount",
+    "allauth",
+    "allauth.account",
+    "dj_rest_auth.registration",
+    "anymail",  # For email verification
     "users",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "allauth.account.middleware.AccountMiddleware", # Required for django-allauth
+    "allauth.account.middleware.AccountMiddleware",  # Required for django-allauth
 ]
 
-ROOT_URLCONF = 'musicplayer_project.urls'
+ROOT_URLCONF = "musicplayer_project.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'musicplayer_project.wsgi.application'
+WSGI_APPLICATION = "musicplayer_project.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
@@ -107,16 +105,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -124,9 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -136,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
@@ -148,53 +146,52 @@ if deployed_frontend:
     CORS_ALLOWED_ORIGINS.append(deployed_frontend)
 
 # Auth settings
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = "users.CustomUser"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    )
+    "DEFAULT_AUTHENTICATION_CLASSES": ("dj_rest_auth.jwt_auth.JWTCookieAuthentication",)
 }
 
 REST_AUTH = {
-    'USE_JWT': True,
-    'JWT_AUTH_COOKIE': 'musicplayer-auth',
-    'JWT_AUTH_REFRESH_COOKIE': 'musicplayer-refresh',
-    'JWT_AUTH_HTTPONLY': True,
-    'USER_DETAILS_SERIALIZER': 'musicplayer.serialisers.CustomUserSerialiser',
+    "USE_JWT": True,
+    "JWT_AUTH_COOKIE": "musicplayer-auth",
+    "JWT_AUTH_REFRESH_COOKIE": "musicplayer-refresh",
+    "JWT_AUTH_HTTPONLY": True,
+    "USER_DETAILS_SERIALIZER": "musicplayer.serialisers.CustomUserSerialiser",
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),  # Short life for security
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),    # Long life for convenience
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),  # Short life for security
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),  # Long life for convenience
 }
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
+    "django.contrib.auth.backends.ModelBackend",
     # `allauth` specific authentication methods, such as login by email
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 # Required for django-allauth (used by dj-rest-auth for registration)
 SITE_ID = 1
 
-# Commented out config as they break auth. Reverted to deprecated settings for compatibility with default User model. 
-# ACCOUNT_LOGIN_METHODS = {'email'}
-# ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*', 'password2*']
-ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_USERNAME_REQUIRED = False
-ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_LOGIN_METHODS = {'email'}
+ACCOUNT_SIGNUP_FIELDS = ['username','email*', 'password1*', 'password2*']
 ACCOUNT_UNIQUE_EMAIL = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory' # Or 'none' for no verification, 'optional' for optional verification
+ACCOUNT_EMAIL_VERIFICATION = (
+    "mandatory"  # Or 'none' for no verification, 'optional' for optional verification
+)
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True  # No need to sent POST request to confirmation link
-LOGIN_URL = "/admin" # Path users will be redirected to after email verification/if not authenticated
+LOGIN_URL = "/admin"  # Path users will be redirected to after email verification/if not authenticated
 
 # Email backend
 ANYMAIL = {
-    "SENDGRID_API_KEY": os.getenv('SENDGRID_API_KEY'),
+    "SENDGRID_API_KEY": os.getenv("SENDGRID_API_KEY"),
 }
 EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
-DEFAULT_FROM_EMAIL = "musicplayer_noreply@proton.me"  # if you don't already have this in settings
-SERVER_EMAIL = "musicplayer_noreply@proton.me"  # ditto (default from-email for Django errors)
+DEFAULT_FROM_EMAIL = (
+    "musicplayer_noreply@proton.me" 
+)
+SERVER_EMAIL = (
+    "musicplayer_noreply@proton.me" 
+)
