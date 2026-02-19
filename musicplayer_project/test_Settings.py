8d42eebecfc94cd2bd5a1c4e51135c8845c68a9c
Poppy Carpenter <poppy.carpenter@ada.ac.uk>
2026-02-19 13:33:41 +0000

    chore: ignore wildcard settings import; required for overriding settings. Fix test run line in CI pipeline

--- musicplayer_project/test_Settings.py
+++ musicplayer_project/test_Settings.py
@@ -1,4 +1,5 @@
-from .settings import *
+# This file imports all settings from the main settings file and then overrides specific settings for testing.
+from .settings import *  # noqa: F403
 
 DATABASES = {
     'default': {
