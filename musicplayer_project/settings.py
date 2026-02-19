9befc4d4d4b478a5a24281b556d194ab6a0440dc
Poppy Carpenter <poppy.carpenter@ada.ac.uk>
2026-02-19 13:48:23 +0000

    chore: add STATIC_ROOT var for deployment

--- musicplayer_project/settings.py
+++ musicplayer_project/settings.py
@@ -134,7 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static/"    
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
