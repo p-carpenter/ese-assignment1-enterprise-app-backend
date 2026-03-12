43ee0bc57e9670a5945e88497c175eaaf5d48904
Poppy Carpenter <poppy.carpenter@ada.ac.uk>
2026-03-12 10:12:45 +0000

    chore(db): Track all migration files

---
+++ musicplayer/migrations/0004_playlistsong_added_by.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("musicplayer", "0003_playlist_is_collaborative"),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="playlistsong",
+            name="added_by",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="playlist_songs_added",
+                to=settings.AUTH_USER_MODEL,
+            ),
+        ),
+    ]
