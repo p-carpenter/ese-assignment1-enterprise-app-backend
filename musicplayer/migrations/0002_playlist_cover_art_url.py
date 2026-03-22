402740133390f208a193db35c203a5c8996d3801
Poppy Carpenter <poppy.carpenter@ada.ac.uk>
2026-03-22 14:45:20 +0000

    chore: commit migrations

--- musicplayer/migrations/0002_playlist_cover_art_url.py
+++ musicplayer/migrations/0002_playlist_cover_art_url.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('musicplayer', '0001_initial'),
+        ("musicplayer", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='playlist',
-            name='cover_art_url',
+            model_name="playlist",
+            name="cover_art_url",
             field=models.URLField(blank=True),
         ),
     ]
